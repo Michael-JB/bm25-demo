77e04f91d94cb32ba58e19b08eaa9171cd278c8e
Michael B <michael@ramyun.co.uk>
2024-12-07 23:16:42 +0100

    Complete initial implementation

---
+++ README.md
@@ -0,0 +1,22 @@
+# bm25-demo
+
+A browser-based demo of the [bm25 Rust crate](https://github.com/Michael-JB/bm25).
+
+This demo is hosted at [https://michael-jb.github.io/bm25-demo](https://michael-jb.github.io/bm25-demo).
+
+## Development
+
+```bash
+cargo install --locked trunk
+trunk serve --open
+```
+
+## Built with
+
+- [Leptos](https://github.com/leptos-rs/leptos) - Web application framework
+- [Trunk](https://github.com/trunk-rs/trunk) - WASM application bundler
+
+## License
+
+[MIT license](./LICENSE)
+
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,122 @@
-use bm25::{Document, Language, SearchEngine, SearchEngineBuilder};
-use gloo_net::http::Request;
-use leptos::prelude::*;
-
+use bm25::{Document, Language, SearchEngine, SearchEngineBuilder, SearchResult};
 use csv::Reader;
-use web_sys::console;
+use gloo_net::http::Request;
+use leptos::{either::Either, prelude::*};
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct Recipe {
     pub title: String,
-    pub recipe: String,
+    pub method: String,
 }
 
 impl From<Recipe> for Document<String> {
     fn from(value: Recipe) -> Self {
-        Document::new(value.title, value.recipe)
+        Document::new(value.title, value.method)
     }
 }
 
-async fn load_data() -> Vec<Recipe> {
-    let response = Request::get("/data/recipes_en.csv")
+async fn load_recipes() -> Vec<Recipe> {
+    let response = Request::get("recipes.csv")
         .send()
         .await
-        .expect("recipes_en.csv");
-    let text = response.text().await.expect("text");
+        .expect("recipes to be available");
+    let text = response.text().await.expect("recipes to be text");
     let mut reader = Reader::from_reader(text.as_bytes());
     reader
         .records()
-        .map(|r| r.unwrap())
+        .map(|r| r.expect("record"))
         .map(|r| {
             let title = r.get(0).expect("title").to_string();
-            let recipe = r.get(1).expect("recipe").to_string();
-            Recipe { title, recipe }
+            let method = r.get(1).expect("method").to_string();
+            Recipe { title, method }
         })
         .collect()
 }
 
 async fn get_search_engine() -> SearchEngine<String, u32> {
-    let recipes = load_data().await;
+    let recipes = load_recipes().await;
     SearchEngineBuilder::with_documents(Language::English, recipes).build()
 }
 
 #[component]
-fn App() -> impl IntoView {
+pub fn App() -> impl IntoView {
+    let (results, set_results) = signal((String::new(), Vec::<SearchResult<String>>::new()));
+
     let search_engine = LocalResource::new(move || get_search_engine());
-    // we can access the resource values with .get()
-    // this will reactively return None before the Future has resolved
-    // and update to Some(T) when it has resolved
-    let search = move |query: &str| {
-        search_engine
-            .read()
-            .as_deref()
-            .map(|value| {
-                let search_engine = value;
-                let results = search_engine.search(query, 5);
-                let results = results
-                    .iter()
-                    .map(|result| result.document.contents.clone())
-                    .collect::<Vec<_>>();
-                format!("{:?}", results)
-            })
-            // This loading state will only show before the first load
-            .unwrap_or_else(|| "Loading...".into())
+
+    let search = move |query: String| {
+        search_engine.read().as_deref().map(|se| {
+            let search_results = se.search(&query, 5);
+            set_results.set((query, search_results));
+        });
     };
 
-    let (result, set_result) = signal("None".to_string());
-
     view! {
-        <input type="text"
-            // adding :target gives us typed access to the element
-            // that is the target of the event that fires
-            on:input:target=move |ev| {
-                set_result.set(search(&ev.target().value()));
-            }
+        <div class="inner-body">
+            <header>
+                <h1>"bm25 demo"</h1>
+                <p>
+                    "This is a demo of the keyword search engine from the "
+                    <a href="https://github.com/Michael-JB/bm25">"bm25"</a>" Rust crate.
+                    Use the search bar below to query a small collection of recipes.
+                    For example, try searching for 'cinnamon' or 'noodle'. Only the top 5 results
+                    are shown. The recipe titles are not included in the search. These recipes are
+                    AI-generated; I do not recommend trying them at home..."
+                </p>
+                <p>
+                    "This demo is written in Rust and compiles to WebAssembly to run directly
+                    in the browser. See the "<a href="https://github.com/Michael-JB/bm25-demo">"source
+                     code"</a>"."
+                </p>
+            </header>
+            <input
+                class="search-input"
+                type="search"
+                placeholder="Search for a recipe..."
+                on:input:target=move |ev| {
+                    search(ev.target().value());
+                }
+            />
 
-            //// the `prop:` syntax lets you update a DOM property,
-            //// rather than an attribute.
-            //prop:value=
-        />
-        <p>"Result is: " {result}</p>
+            {move || {
+                let (ref query, ref search_results) = *results.read();
+
+                if query.is_empty() {
+                    return None;
+                }
+
+                let table_head = view! {
+                    <tr>
+                        <th style="width: 8%">"Score"</th>
+                        <th style="width: 92%">"Recipe"</th>
+                    </tr>
+                };
+                let table_body = if search_results.is_empty() {
+                    Either::Left(view! {<tr><td colspan="2">"No results."</td></tr>})
+                } else {
+                    Either::Right(search_results.iter().map(|result| {
+                        view! {
+                            <tr>
+                                <td>{format!("{:.3}", result.score.clone())}</td>
+                                <td>
+                                    <h1 class="recipe-title">{result.document.id.clone()}</h1>
+                                    <p class="recipe-contents">{result.document.contents.clone()}</p>
+                                </td>
+                            </tr>
+                        }
+                    }).collect::<Vec<_>>())
+                };
+                Some(view! {
+                    <table class="results-table">
+                        <thead>{table_head}</thead>
+                        <tbody>{table_body}</tbody>
+                    </table>
+                })
+            }}
+        </div>
     }
 }
 
 fn main() {
-    console_error_panic_hook::set_once();
     mount_to_body(App);
 }
